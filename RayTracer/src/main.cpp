5d36c857477fbd59e35288bd9142953ca6077300
Kaisa970 <magicoftotoro@163.com>
2023-07-27 13:18:11 +0800

    Using multithreading acceleration

--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -5,8 +5,13 @@
 #include "Sphere.h"
 #include "Random.h"
 #include "Material.h"
+#include <mutex>
 
 const float MAXFLOAT = 100000;
+const int nx = 256;
+const int ny = 128;
+const int ns = 100;
+const int num_threads = 14;
 
 vec3 getColor(const Ray& ray,Hitable* scene,int depth) {
 	
@@ -16,7 +21,7 @@ vec3 getColor(const Ray& ray,Hitable* scene,int depth) {
 		vec3 target = hr.p + hr.normal + RandomPointInUnitSphere();
 		Ray scattered;
 		vec3 attenuation;
-		if (depth<10 && 
+		if (depth<50 && 
 			hr.material->Scatter(ray, hr, attenuation, scattered))
 		{
 			return attenuation * getColor(scattered, scene, depth + 1);
@@ -31,11 +36,36 @@ vec3 getColor(const Ray& ray,Hitable* scene,int depth) {
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
+void renderPixels(int thread_id, Hitable* scene, Camera& cam,
+	vec3(&img)[nx][ny], std::mutex& mtx) {
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = thread_id; i < nx; i+=num_threads)
+		{
+			vec3 col(0, 0, 0);
+			for (int k = 0; k < ns; k++)
+			{
+				float u = float(i + GetRandom01()) / float(nx);
+				float v = float(j + GetRandom01()) / float(ny);
+				Ray ray = cam.GetRay(u, v);
+				col += getColor(ray, scene, 0);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));// gamma½ÃÕý,gamma = 2
+			int ir = int(255.99 * col.r());
+			int ig = int(255.99 * col.g());
+			int ib = int(255.99 * col.b());
+
+			img[i][j] = vec3(ir, ig, ib);
+		}
+	}
+}
+
 
 int main() {
-	int nx = 256;
-	int ny = 128;
-	int ns = 10;
+
+
+	vec3 img[nx][ny];
 
 	std::ofstream fs("output.ppm");
 
@@ -52,25 +82,23 @@ int main() {
 
 	Hitable* scene = new HitableList(objList, 4);
 
-	for (int j = ny - 1; j >= 0; j--)
-	{
-		for (int i = 0; i < nx; i++)
-		{
-			vec3 col(0, 0, 0);
-			for (int k = 0; k < ns; k++)
-			{
-				float u = float(i + GetRandom01()) / float(nx);
-				float v = float(j + GetRandom01()) / float(ny);
-				Ray ray = cam.GetRay(u, v);
-				col += getColor(ray, scene,0);
-			}
-			col /= float(ns);
-			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));// gamma½ÃÕý,gamma = 2
-			int ir = int(255.99 * col.r());
-			int ig = int(255.99 * col.g());
-			int ib = int(255.99 * col.b());
+	std::thread threads[num_threads];
+	std::mutex mtx;
 
-			fs<< ir << " " << ig << " " << ib << "\n";
+	for (int i = 0; i < num_threads; i++)
+	{
+		threads[i] = std::thread(renderPixels, i, scene, std::ref(cam), 
+			std::ref(img), std::ref(mtx));
+	}
+	
+	for (int i = 0; i < num_threads; i++)
+	{
+		threads[i].join();
+	}
+
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			fs << int(img[i][j].r()) << " " << int(img[i][j].g()) << " " << int(img[i][j].b()) << "\n";
 		}
 	}
 
